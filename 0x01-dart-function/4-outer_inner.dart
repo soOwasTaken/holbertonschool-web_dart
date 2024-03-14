26fc5850d859b2f02890b33953eda3e7caf66214
soOwasTaken <444deph12@gmail.com>
2024-03-15 00:30:05 +0100

    change has to be made

---
+++ 0x01-dart-function/4-outer_inner.dart
@@ -0,0 +1,11 @@
+String inner (String name) {
+    List<String> nameParts = name.split(" ");
+    String lastNameInitial = nameParts[0][0].toUpperCase();
+    String firstName = nameParts[1];
+    return "Hello Agent $lastNameInitial.$firstName";
+  }
+
+Void outer(String name, String id) {
+    String result = inner(name);
+    print("$result your id is $id");
+  }
